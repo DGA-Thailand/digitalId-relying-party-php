fb1087501468dec98a853e8f9b3056b5610aae2a
pairoj <pairoj.choochedduangtinakorn@ega.or.th>
2021-07-18 21:27:33 +0700

    Upload New File

---
+++ oidc_logout.php
@@ -0,0 +1,32 @@
+<?php
+require 'vendor/autoload.php';
+require_once "Utility.php"; // EGA Utility
+require_once "Config.php"; // config
+
+session_start();
+
+if(isset($_SESSION['idToken']) && !empty($_SESSION['idToken'])) {
+	echo 'Set and not empty, and no undefined index error!';
+
+	$idToken = $_SESSION['idToken'];
+	//print_r($idToken);
+	session_destroy();
+	
+	$issuer = $issuerURL;
+	$cid = $clientID;
+	$secret = Utility::prepareSecret($clientSecret);
+	$oidc = new Jumbojett\OpenIDConnectClient($issuer, $cid, $secret);
+	//$oidc->setRedirectURL('http://localhost:8090/oidc_client/oidc_login.php');
+	$oidc->setVerifyHost(false); // locally
+	$oidc->setVerifyPeer(false); // locally
+
+
+	$oidc->signOut($idToken, $logoutCallbackURL);
+
+}
+else
+{
+	
+	?><a href="index.php">กลับหน้าหลัก</a><?php
+}
+?>
