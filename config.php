b032b76c0a232ed46b4dba96489b8c8c2dff554c
pairoj <pairoj.choochedduangtinakorn@ega.or.th>
2021-07-18 21:26:26 +0700

    Upload New File

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$issuerURL = "https://connect.dga.or.th";
+$clientID = "89ee233d-0dbc-4f28-91f3-17772220cd76";
+$clientSecret = "G7kI60eI2Vr";
+
+//client url
+$loginCallbackURL = "http://localhost:8090/oidc_client/oidc_login.php";
+$logoutCallbackURL = "http://localhost:8090/oidc_client/oidc_logout.php";
+?>
