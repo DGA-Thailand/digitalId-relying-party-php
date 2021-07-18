60d6f68b12661c51dcf523dbe0a8b52d729caf79
pairoj <pairoj.choochedduangtinakorn@ega.or.th>
2021-07-18 21:27:11 +0700

    Upload New File

---
+++ oidc_login.php
@@ -0,0 +1,57 @@
+<?php
+require 'vendor/autoload.php';
+require_once "Utility.php"; // EGA Utility
+require_once "Config.php"; // config
+
+session_start();
+
+$issuer = $issuerURL;
+$cid = $clientID;
+$secret = Utility::prepareSecret($clientSecret);
+$oidc = new Jumbojett\OpenIDConnectClient($issuer, $cid, $secret);
+
+$oidc->setVerifyHost(false);
+$oidc->setVerifyPeer(false);
+
+$oidc->setResponseTypes(array('code'));
+$oidc->addScope(array('openid',
+	'personal_token',
+	'given_name',
+	'citizen_id',
+	'citizen_id_verified',
+	'email',
+	'email_verified',
+	'phone_number',
+	'phone_number_verified',
+	'preferred_username','ial_level')
+);
+$oidc->setRedirectURL($loginCallbackURL);
+ 
+$oidc->setCodeChallengeMethod('S256');
+
+$oidc->authenticate();
+$oidc->requestUserInfo();
+$userInfo = $oidc->requestUserInfo();
+//$accessToken = $oidc->getAccessToken();
+$idToken = $oidc->getIdToken();
+//print_r($oidc);
+//print_r($accessToken);
+
+$session = array();
+foreach($userInfo as $key=> $value) {
+    if(is_array($value)){
+            $v = implode(', ', $value);
+    }else{
+            $v = $value;
+    }
+    $session[$key] = $v;
+}
+ 
+ 
+//$_SESSION['oidc'] = $oidc;
+$_SESSION['attributes'] = $session;
+$_SESSION['idToken'] = $idToken; 
+
+header("Location: ./attributes.php");
+ 
+?>
