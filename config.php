5104bc6904ef137929c0365e07b194dd7e77d6d5
pairoj <pairoj.choochedduangtinakorn@ega.or.th>
2021-07-18 22:20:22 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,11 @@
 <?php
-$issuerURL = "https://connect.dga.or.th";
-$clientID = "89ee233d-0dbc-4f28-91f3-17772220cd76";
-$clientSecret = "G7kI60eI2Vr";
+//$issuerURL = "https://connect.egov.go.th";//for production
+$issuerURL = "https://connect.dga.or.th";//for test
+$clientID = "";
+$clientSecret = "";
 
 //client url
-$loginCallbackURL = "http://localhost:8090/oidc_client/oidc_login.php";
-$logoutCallbackURL = "http://localhost:8090/oidc_client/oidc_logout.php";
+$domain = "YOUR DOMAIN";
+$loginCallbackURL = $domain."/oidc_login.php";
+$logoutCallbackURL = $domain."/oidc_logout.php";
 ?>
